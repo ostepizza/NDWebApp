410af570a4fff7b4169bfd035c84c91b4ec778de
mnilsen <32636970+ostepizza@users.noreply.github.com>
2022-11-11 21:31:36 +0100

    Konvertert gammel login
    Konvertert gammel login til ny på area/identy
    +
    forsøk på å lage egen login på /login

--- NDWebApp/Controllers/HomeController.cs
+++ NDWebApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using NDWebApp.Models;
 using System.Diagnostics;
 
@@ -13,6 +14,7 @@ namespace NDWebApp.Controllers
             _logger = logger;
         }
 
+        [Authorize]
         public IActionResult Index()
         {
             return View();
---
+++ NDWebApp/Models/LoginModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace NDWebApp.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+        public string LoginInvalid { get; set; }
+    }
+}
