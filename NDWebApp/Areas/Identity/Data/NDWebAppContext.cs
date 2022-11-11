fd194a556bb81fad4c306538153a1c4d4a5e22f3
mnilsen <32636970+ostepizza@users.noreply.github.com>
2022-11-11 12:55:28 +0100

    Initial commit

---
+++ NDWebApp/Areas/Identity/Data/NDWebAppContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NDWebApp.Areas.Identity.Data;
+
+namespace NDWebApp.Data;
+
+public class NDWebAppContext : IdentityDbContext<NDWebAppUser>
+{
+    public NDWebAppContext(DbContextOptions<NDWebAppContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
