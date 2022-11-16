6cc14c7706807ead71e970ab8c93863afa32f1e3
Lukas <89383438+Lennern@users.noreply.github.com>
2022-11-16 11:41:31 +0100

    Migrations for Team, Suggestion and Repairs

---
+++ NDWebApp/Migrations/20221114110235_AddTeams.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NDWebApp.Migrations
+{
+    public partial class AddTeams : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+             migrationBuilder.CreateTable(
+                name: "Team",
+                columns: table => new
+                {
+                    TeamId = table.Column<string>(type: "int", nullable: false),
+                    TeamName = table.Column<string>(type: "varchar(256)", maxLength: 256, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LeaderUserId = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Team", x => x.TeamId);
+                    table.ForeignKey(
+                        name: "FK_LeaderUserId",
+                        column: x => x.LeaderUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+               name: "TeamId",
+               table: "AspNetUsers",
+               type: "int",
+               nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Team");
+
+            migrationBuilder.DropColumn(
+                name: "TeamId",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ NDWebApp/Migrations/20221115114412_SuggestionsAndRepairs.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NDWebApp.Migrations
+{
+    public partial class SuggestionsAndRepairs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+               name: "Status",
+               columns: table => new
+               {
+                   StatusId = table.Column<string>(type: "int", nullable: false),
+                   StatusTitle = table.Column<string>(type: "varchar(256)", maxLength: 256, nullable: false)
+                       .Annotation("MySql:CharSet", "utf8mb4"),
+               },
+               constraints: table =>
+               {
+                   table.PrimaryKey("PK_Status", x => x.StatusId);
+               })
+               .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Suggestion",
+                columns: table => new
+                {
+                    SuggestionId = table.Column<string>(type: "int", nullable: false),
+                    SuggestionTitle = table.Column<string>(type: "varchar(256)", maxLength: 256, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SuggestionDescription = table.Column<string>(type: "text", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SuggestionDeadline = table.Column<string>(type: "datetime", nullable: true),
+                    SuggestionEnddate = table.Column<string>(type: "datetime", nullable: true),
+                    SuggestedUserId = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ResponibleUserId = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    TeamId = table.Column<string>(type: "int", nullable: true),
+                    StatusId = table.Column<string>(type: "int", nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suggestion", x => x.SuggestionId);
+                    table.ForeignKey(
+                        name: "FK_SuggestedUserId",
+                        column: x => x.SuggestedUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+
+                    table.ForeignKey(
+                        name: "FK_ResponsibleUserId",
+                        column: x => x.ResponibleUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+
+                    table.ForeignKey(
+                        name: "FK_SuggestionStatus",
+                        column: x => x.StatusId,
+                        principalTable: "Status",
+                        principalColumn: "StatusId",
+                        onDelete: ReferentialAction.Cascade);
+
+                    table.ForeignKey(
+                        name: "FK_SuggestionTeam",
+                        column: x => x.TeamId,
+                        principalTable: "Team",
+                        principalColumn: "TeamId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Repairs",
+                columns: table => new
+                {
+                    RepairsId = table.Column<string>(type: "int", nullable: false),
+                    RepairsTitle = table.Column<string>(type: "varchar(256)", maxLength: 256, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    RepairsDescription = table.Column<string>(type: "text", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    RepairsDeadline = table.Column<string>(type: "datetime", nullable: true),
+                    RepairsEnddate = table.Column<string>(type: "datetime", nullable: true),
+                    UserId = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    TeamId = table.Column<string>(type: "int", nullable: true),
+                    StatusId = table.Column<string>(type: "int", nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Repairs", x => x.RepairsId);
+                   
+                    table.ForeignKey(
+                        name: "FK_RepairsUserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+
+                    table.ForeignKey(
+                        name: "FK_RepairStatus",
+                        column: x => x.StatusId,
+                        principalTable: "Status",
+                        principalColumn: "StatusId",
+                        onDelete: ReferentialAction.Cascade);
+
+                    table.ForeignKey(
+                        name: "FK_RepairTeam",
+                        column: x => x.TeamId,
+                        principalTable: "Team",
+                        principalColumn: "TeamId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Suggestions");
+
+            migrationBuilder.DropTable(
+                name: "Repairs");
+            
+            migrationBuilder.DropTable(
+                name: "Status");
+        }
+    }
+}
