cfc2d3dfbcecc756141c4092ea79e76bd39ab68c
mnilsen <32636970+ostepizza@users.noreply.github.com>
2022-11-21 18:07:02 +0100

    Userpanel updates

--- NDWebApp/Areas/Identity/Data/NDWebAppUser.cs
+++ NDWebApp/Areas/Identity/Data/NDWebAppUser.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Microsoft.AspNetCore.Identity;
 
 namespace NDWebApp.Areas.Identity.Data;
@@ -9,8 +12,13 @@ namespace NDWebApp.Areas.Identity.Data;
 // Add profile data for application users by adding properties to the NDWebAppUser class
 public class NDWebAppUser : IdentityUser
 {
+    [DisplayName("Ansattnummer")]
     public int empNr { get; set; }
+
+    [DisplayName("Fornavn")]
     public string empFname { get; set; } = "";
+
+    [DisplayName("Etternavn")]
     public string empLname { get; set; } = "";
 }
 
