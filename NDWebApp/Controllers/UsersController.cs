224182c11bfe12746e0180d67feed1b7a8660d27
mnilsen <32636970+ostepizza@users.noreply.github.com>
2022-11-21 15:59:41 +0100

    Added some user management
    - Can view user list
    - List only accessible by TL and Admin
    - Can view users
    - View only accessible by TL and Admin
    - Can add, update and delete users somewhat
    - Only accessible by Admin

--- NDWebApp/Controllers/UsersController.cs
+++ NDWebApp/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using NDWebApp.Areas.Identity.Data;
 using NDWebApp.Models;
 using System.Diagnostics;
 
@@ -7,14 +9,137 @@ namespace NDWebApp.MVC.Controllers
 {
     public class UsersController : Controller
     {
+        private UserManager<NDWebAppUser> userManager;
+        private IPasswordHasher<NDWebAppUser> passwordHasher;
+
+        public UsersController(UserManager<NDWebAppUser> usrMgr, IPasswordHasher<NDWebAppUser> passwordHash)
+        {
+            userManager = usrMgr;
+            passwordHasher = passwordHash;
+        }
+
+        [Authorize(Roles = "Administrator,Team Leader")]
         public IActionResult Index()
+        {
+            return View(userManager.Users);
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public ViewResult Add() => View();
+
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
+        public async Task<IActionResult> Add(UserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                NDWebAppUser appUser = new NDWebAppUser
+                {
+                    empNr = user.empNr,
+                    empFname = user.empFname,
+                    empLname = user.empLname,
+                    UserName = user.Email,
+                    Email = user.Email,
+                    PhoneNumber = user.Phone
+                };
+
+                IdentityResult result = await userManager.CreateAsync(appUser, user.Password);
+
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                        ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(user);
+        }
+
+        public IActionResult Success()
         {
             return View();
         }
 
-        public IActionResult Add()
+        public IActionResult NotFound()
         {
             return View();
         }
+
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> Update(string id)
+        {
+            NDWebAppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+                return View(user);
+            else
+                return RedirectToAction("NotFound");
+        }
+
+        [Authorize(Roles = "Administrator,Team Leader")]
+        public async Task<IActionResult> View(string id)
+        {
+            NDWebAppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+                return View(user);
+            else
+                return RedirectToAction("NotFound");
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
+        public async Task<IActionResult> Update(string id, string email, string password)
+        {
+            NDWebAppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                if (!string.IsNullOrEmpty(email))
+                    user.Email = email;
+                else
+                    ModelState.AddModelError("", "Email cannot be empty");
+
+                if (!string.IsNullOrEmpty(password))
+                    user.PasswordHash = passwordHasher.HashPassword(user, password);
+                else
+                    ModelState.AddModelError("", "Password cannot be empty");
+
+                if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
+                {
+                    IdentityResult result = await userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                        return RedirectToAction("Index");
+                    else
+                        Errors(result);
+                }
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+            return View(user);
+        }
+
+        private void Errors(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("", error.Description);
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            NDWebAppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await userManager.DeleteAsync(user);
+                if (result.Succeeded)
+                    return RedirectToAction("Success");
+                else
+                    Errors(result);
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+            return View("Index", userManager.Users);
+        }
+
     }
 }
---
+++ NDWebApp/Models/UserModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NDWebApp.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string Phone { get; set; }
+
+        [Required]
+        public int empNr { get; set; }
+
+        [Required]
+        public string empFname { get; set; } = "";
+
+        [Required]
+        public string empLname { get; set; } = "";
+    }
+}
