5994673ca66d8b002799961d276070c2b97281fc
mnilsen <32636970+ostepizza@users.noreply.github.com>
2022-11-25 02:50:28 +0100

    Removed unused stuff

--- NDWebApp/Areas/Admin/Controllers/HomeController.cs
+++ NDWebApp/Areas/Admin/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 namespace NDWebApp.MVC.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Administrator")]
     public class HomeController : Controller
     {
         public IActionResult Index()
